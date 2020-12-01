dd4c5ba7ab7f87264d8dc00421f36ffe62101a86
sonalijain <sonalij460@gmaul.com>
2020-12-01 13:28:37 +0530

    project setup

---
+++ src/Demo/Footer.js
@@ -0,0 +1,18 @@
+import react from "react";
+import React from "react";
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        fontSize: 24,
+        padding: 10,
+        fontStyle: "italic",
+      }}
+    >
+      Sonali Jain - Sonalijain057@gmil.com - Referance and Request
+    </div>
+  );
+};
+
+export default Footer;
