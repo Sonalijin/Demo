0d8e9f5fe4b7c661c1cb42e99c044572b363d093
navin <naveenprakash74@gmail.com>
2020-12-16 00:31:54 +0530

    Login Page created

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 // import Navbar from './components/Navbar'
 import MyResume from "./Demo";
+import Login from "./Login";
+
+import * as serviceWorker from './serviceWorker';
 
 const App = (props) => {
-  return <MyResume {...props} />;
+  return <Login {...props} />;
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
