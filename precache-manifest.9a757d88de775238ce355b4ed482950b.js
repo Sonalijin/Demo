77044e54aabaeb0facd095f2f20d14f0c8c63c30
Sonalijin <75158499+Sonalijin@users.noreply.github.com>
2020-12-31 14:12:23 +0530

    Updates

---
+++ precache-manifest.9a757d88de775238ce355b4ed482950b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c596b439dbecbd71dd40bf7e45267122",
+    "url": "/Demo/index.html"
+  },
+  {
+    "revision": "25bbc223927ae53fc902",
+    "url": "/Demo/static/css/main.7947f850.chunk.css"
+  },
+  {
+    "revision": "ba800aec35e3d931256a",
+    "url": "/Demo/static/js/2.2d64cb8e.chunk.js"
+  },
+  {
+    "revision": "25bbc223927ae53fc902",
+    "url": "/Demo/static/js/main.c17afb85.chunk.js"
+  },
+  {
+    "revision": "a9b807591dac23aa0c24",
+    "url": "/Demo/static/js/runtime-main.25d3a0ec.js"
+  }
+]);
